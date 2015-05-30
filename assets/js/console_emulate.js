f6b37f0e753e4d6c12ac59102eac73ddae6afbba
Andrew Switlyk <AndrewSwitlyk@Emilys-MacBook-Pro.local>
2015-05-30 01:19:12 -0700

    feature - added iteration of JSON code object and improved text input to screen to better resemble typing

--- assets/js/console_emulate.js
+++ assets/js/console_emulate.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 		// one line
 			'<span class="code-line"></span>',
 			'<span class="code-var"></span>',
-			'class &nbsp',
+			'class ',
 			'<span class="code-white"></span>',
 			'Engineer():',
 		// two lines
@@ -19,24 +19,23 @@ $(document).ready(function() {
 			'<span class="code-arg"></span>',
 			'self',
 			'<span class="code-white"></span>',
-			',&nbsp;*',
+			', *',
 			'<span class="code-arg"></span>',
 			'args',
 			'<span class="code-white"></span>',
 			'):'
 		],
 
-		javascript: [
-
-
+		'javascript': [
+			'<span class="code-line"></span>',
+			'<span class="code-var"></span>',
+			'class ',
+			'<span class="code-white"></span>',
+			'Engin',
+			'eer():',
 		]
+	};
 
-	}
-
-	// accepts an array of strings
-	function processLangArr() {
-
-	}
 
 	// accepts a string to see if it starts with <span
 	function checkForSpan(str) {
@@ -62,22 +61,18 @@ $(document).ready(function() {
 		var span = $('.code-line:last');
 	}
 
-	function appendHTMLtoEl(el) {
-
-	}
-
-	function insertHTMLintoEl(el) {
-
-	}
-
-
 	// get text cursor
 	var txtcursor = $('#txtcursor');
 	// make cursor blink
+
+
+	function makeCursorBlink() {
+
+	}
+
 	var intervalId = setInterval(function(tc) {
 		var hidden = false;
 		return function() {
-			console.log()
 			if(!hidden) {
 				tc.hide();
 				hidden = true;
@@ -94,56 +89,129 @@ $(document).ready(function() {
 		codeContainerNode = $('.code-container'),
 		txtCursorNode = $('#txtcursor');
 
+	var intervalTime = 500;
+
+	function addBlockToCurrentSpanLine() {
+		currentSpanLine.addClass("code-block");
+	}
+
+	function addCodeBlockClassToEL(el) {
+		el.addClass("code-block");
+	}
+
 	// Parse items in array
-
-
 	function parseArrayItems(v, i, a) {
 
 		if(checkForSpan(v)) {
-					// is value a span class="code-line"
-					if(checkSpanForClassLine(v)) {
-						// if not first currentSpanLine, set add block class to current Span Line before reassigning
-						if(currentSpanLine) {
-							currentSpanLine.addClass("code-block");
+			// is value a span class="code-line"
+			if(checkSpanForClassLine(v)) {
+				// if not first currentSpanLine, set add block class to current Span Line before reassigning
+				if(currentSpanLine) {
+
+					setTimeout(function(el) {
+						return function() {
+							addCodeBlockClassToEL(el);
 						}
-						currentSpanLine = $(v);
-						// insert before txtCursor
-						txtCursorNode.before(currentSpanLine);
+					}(currentSpanLine), intervalTime+50);
 
+					// currentSpanLine.addClass("code-block");
+				}
+				currentSpanLine = $(v);
+				// insert before txtCursor
+				txtCursorNode.before(currentSpanLine);
 
-					} else {
-						// insert span into current Span Line
-						currentInnerSpan = $(v);
-						currentSpanLine.append(currentInnerSpan);
+			} else {
+				// insert span into current Span Line
+				currentInnerSpan = $(v);
+				currentSpanLine.append(currentInnerSpan);
+			}
+
+		} else {  // will be a string of characters, insert them into currentInnerSpan
+			// add each character in on at a time
+			var charInterval = v.length > 6 ? Math.floor( intervalTime/ v.length) : 100;
+			for(var j = 0; j < v.length; j++) {
+				setTimeout(function(ch) {
+					var character = ch;
+					return function() {
+						currentInnerSpan.append(character);
 					}
+				}(v.charAt(j)), 0 + (charInterval * j));
+			}
+			// currentInnerSpan.html(v);
+		}
 
-				} else {  // will be a string of characters, insert them into currentInnerSpan
-					currentInnerSpan.html(v);
-				}
+		// if end of array add class code-block to currentSpanLine
+		if(i == a.length - 1) {
+			// setTimeout(addBlockToCurrentSpanLine, intervalTime);
+			// currentSpanLine.addClass("code-block");
+			// setTimeout(codeContainerChildrenFade, 500);
 
-				// if end of array add class code-block to currentSpanLine
-				if(i == a.length - 1) {
-					currentSpanLine.addClass("code-block");
-				}
+		}
 	}
 
-	function testIt() {
 
-		codeObj.python.forEach(function(v, i, a) {
+	// iterates through the array and parse's each array value
+	function iterateArr(arr) {
 
-			// setTimeout will call a function for each iteration
-			// with it's own closure
+		arr.forEach(function(v, i, a) {
 			setTimeout(function(value, index, arr) {
 				return function() {
-					parseArrayItems(value, index, arr);
+					parseArrayItems(value, index, arr)
 				}
-			}(v, i, a), 500 + (i * 500));
-
+			}(v, i, a), 500 + (i * intervalTime));
 		});
-	} // end of testIt
+	}
 
-	// test it out after three seconds
-	setTimeout(testIt, 3000);
+	// global variable to store the last delay time so that
+	// tDelay will be correct (subtract last delay time from tDelay)
+	// since tDelay adds it twice 
+	var lastDelay = 0;
+	var firstTime = true;
+
+	// will delay the running of the iterateArr function
+	function delayArrIteration(arr, delay) {
+		// need to add time after first time run to allow for fadeaway on next array iteration
+		var delay = firstTime ? delay : delay + 6000
+		firstTime = false;
+		setTimeout(iterateArr, delay, arr);
+		// setTimeout(codeContainerChildrenFade, delay + 500);
+		var addToDelay = (arr.length - 1) * intervalTime;
+		var delayTime = delay + addToDelay;
+		lastDelay = addToDelay;
+		return delayTime;
+	}
+
+	function codeContainerChildrenFade() {
+		codeContainerNode.children().fadeOut(1500, function() {
+			// codeContainerNode.html(txtCursorNode)
+		});
+		setTimeout(function() {
+			return function() {
+				codeContainerNode.html(txtCursorNode);
+			}
+		}(), 1500)
+	}
+
+	// will iterate through the global codeObj JSON object
+	// and when done, run itself again after a delay
+	function iterateCodeObj() {
+		var timeoutId;
+		var tDelay = 0;
+		for(key in codeObj) {
+			tDelay += delayArrIteration(codeObj[key], tDelay);
+			var fadeDelay = tDelay + 4000;
+			timeoutId = setTimeout(codeContainerChildrenFade, fadeDelay);
+		}
+		// setTimeout(codeContainerChildrenFade, tDelay - (lastDelay + 2000));
+		// setTimeout(iterateCodeObj, tDelay - lastDelay + 2500);
+		clearTimeout(timeoutId);
+		setTimeout(codeContainerChildrenFade, tDelay - lastDelay - 1000);
+		setTimeout(iterateCodeObj, tDelay - lastDelay + 1000);
+		firstTime = true;
+	}
+
+	// iterate the code Object
+	setTimeout(iterateCodeObj, 10);
 
 }); // end of document.ready
 
